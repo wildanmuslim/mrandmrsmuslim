fd5491d2c319925ea85868340c466815854f7216
WildanMuslim <wildan16muslim@gmail.com>
2017-07-25 15:02:33 +0700

    init commit

---
+++ application/views/vopening.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<!--
+	Eventually by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Eventually by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="<?php echo base_url('html/opening/assets/css/main.css')?>" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1>We are Getting Merried</h1>
+				<p>First come love now come marriage. <br> 
+				"In the end, your heart will find the right home" <br>
+					<a href="#">-71017-</a></p>
+			</header>
+
+		
+
+		<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; mrandmrsmuslim.com</li><li>Photo by: <a href="https://www.instagram.com/fireworks_photography/" target="_blank">Fireworks Photography</a></li>
+				</ul>
+			</footer>
+
+		<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="<?php echo base_url('html/opening/assets/js/main.js')?>"></script>
+
+	</body>
+</html>
